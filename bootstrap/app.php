59ef0f604383b91571666e120295c665db02962d
Ridho Robbi Pasi <ridhorobbipasi@gmail.com>
2026-05-20 05:46:14 +0700

    fix: trust all proxies in bootstrap/app.php to resolve HTTPS redirect loop (ERR_TOO_MANY_REDIRECTS)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,6 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->append(\App\Http\Middleware\CheckMaintenanceMode::class);
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
         $middleware->alias([
